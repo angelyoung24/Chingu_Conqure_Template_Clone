5425be64b32cd3a56fbeaa23ad481efb8a1537f5
angelyoung24 <angel.m.young24@gmail.com>
2020-03-26 02:56:04 -0400

    made changes in index.js and style.css

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+window.addEventListener('scroll', function (e) {
+  let scrolled = window.pageYOffset;
+  const background = document.querySelector('.parallax');
+  parallax.style.top = - (scrolled * 0.2) + 'px';
+});
